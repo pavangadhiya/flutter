3b8c17bf9e9b44091189c91a0a4bfad260cb68ad
pavangadhiya <91672966+pavangadhiya@users.noreply.github.com>
2022-09-17 22:18:41 +0530

    Update main.dart

--- lab8/tutorial2/main.dart
+++ lab8/tutorial2/main.dart
@@ -1,1 +1,73 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
 
+void main() => runApp(MaterialApp(
+  home: EchoList(),
+));
+
+class EchoList extends StatefulWidget {
+  const EchoList({Key? key}) : super(key: key);
+  @override
+  State<EchoList> createState() => _EchoListState();
+}
+
+class _EchoListState extends State<EchoList> {
+  // List<String> quotes = [
+  //   'The truth is realy pure and never simple',
+  //   'You only live once, but if you do it right, once is enough.',
+  //   'Impossible says I`m possible!!',
+  //   'The time is always right to do what is right'
+  // ];
+
+  List<Quote> quotes = [
+    Quote(text: 'The truth is realy pure and never simple',author: 'pavan1'),
+    Quote(author: 'pavan', text: 'You only live once, but if you do it right, once is enough.'),
+    Quote(author: 'pavan3', text: 'Impossible says I`m possible!!'),
+    Quote(text: 'The time is always right to do what is right',author:'pavan2'),
+  ];
+
+  Widget quoteTemplate(quote){
+    return Card(
+      margin: EdgeInsets.fromLTRB(20.0, 30.0, 40.0, 10.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 26,
+                color: Colors.deepPurple,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Text('Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.tealAccent,
+      ),
+
+      body: Column(
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+      ),
+    );
+  }
+}
