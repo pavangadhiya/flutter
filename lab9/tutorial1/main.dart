ad37ef257a980b512a41ccaf858d715b5e5ccd0a
pavangadhiya <91672966+pavangadhiya@users.noreply.github.com>
2022-09-17 23:04:49 +0530

    Update main.dart

--- lab9/tutorial1/main.dart
+++ lab9/tutorial1/main.dart
@@ -1,1 +1,51 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() => runApp(MaterialApp(
+  home: EchoList(),
+));
+
+class EchoList extends StatefulWidget {
+  const EchoList({Key? key}) : super(key: key);
+  @override
+  State<EchoList> createState() => _EchoListState();
+}
+
+class _EchoListState extends State<EchoList> {
+  List<Quote> quotes = [
+    Quote(text: 'The truth is realy pure and never simple',author: 'pavan1'),
+    Quote(author: 'pavan', text: 'You only live once, but if you do it right, once is enough.'),
+    Quote(author: 'pavan3', text: 'Impossible says I`m possible!!'),
+    Quote(text: 'The time is always right to do what is right',author:
+    'pavan2'),
+  ];
+
+  Widget quoteTemplate(quote){
+    return QuoteCard(quote: quote,delete: (){});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Text('Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.tealAccent,
+      ),
+
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(
+          quote: quote,
+          delete: () {
+            setState(() {
+              quotes.remove(quote);
+            });
+          },
+        )).toList(),
+      ),
+    );
+  }
+}
 
