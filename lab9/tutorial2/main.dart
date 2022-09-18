7578a396d40afb049aff1e62220abc88ad0d26b1
pavangadhiya <91672966+pavangadhiya@users.noreply.github.com>
2022-09-18 21:15:33 +0530

    Update main.dart

--- lab9/tutorial2/main.dart
+++ lab9/tutorial2/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:lab9/pages/home.dart';
+import 'package:lab9/pages/loading.dart';
+import 'package:lab9/pages/choose_location.dart';
 
+void main() => runApp(MaterialApp(
+  // home: Home(),
+  debugShowCheckedModeBanner: false,
+
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    }
+));
