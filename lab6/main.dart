920e5e0b78b80bde6ac0868447c3fc32eaab85e9
pavangadhiya <91672966+pavangadhiya@users.noreply.github.com>
2022-09-19 21:42:22 +0530

    Update main.dart

--- lab6/main.dart
+++ lab6/main.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
 
+class HomeScreen extends StatelessWidget {
+  // const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+void main() => runApp(MaterialApp(
+// home is property and after : is its value widget.
+// Scaffold is widgets built in flutter sdk..Scaffold is one type of
+//layout manager..
+home: Scaffold(
+  appBar: AppBar(
+  title: Text('Hello Flutter My First App'),
+  centerTitle: true,
+),
+
+  body: Center(
+    child: Text('Hello Flutter',
+      style: TextStyle(
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 2.0,
+        color: Colors.grey[600],
+      ),),
+  ),
+
+  floatingActionButton: FloatingActionButton(
+    onPressed: () { },
+    child: Text('Click'),
+  ),
+),
+));
