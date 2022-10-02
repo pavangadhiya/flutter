a84b9dc1a52dbd60a605b22c54b4e69781ee56ec
pavangadhiya <91672966+pavangadhiya@users.noreply.github.com>
2022-10-02 23:39:49 +0530

    Update choose_location.dart

--- Lab11/pages/choose_location.dart
+++ Lab11/pages/choose_location.dart
@@ -5,32 +5,6 @@ class ChooseLocation extends StatefulWidget {
 }
 class _ChooseLocationState extends State<ChooseLocation> {
   int counter = 0;
-  // void getData() async
-  // {
-  //     // Future.delayed(Duration(seconds: 4),(){
-  //     //   print('Hello DDU');
-  //     // });
-  //     // print('in getdata ..after future call...');
-  //   String username = await Future.delayed(Duration(seconds: 4), () {
-  //     return 'UNIVERSITY NAME : DDU';
-  //   });
-  //
-  //   String bio = await Future.delayed(Duration(seconds: 2), () {
-  //     return 'DDU IS ONE OF THE BEST UNIVERSITY OF GUJARAT FOR COMPUTER ENGINEERING STUDY';
-  //   });
-  //
-  //   print('$username -> $bio');
-  // }
-  //
-  // @override
-  // void initState()
-  // {
-  //   super.initState();
-  //   //print('INIT STATE FUNCTION RUN IN CHOOSE LOCATION...');
-  //   print('before gatData call');
-  //   getData();
-  //   print('after getdata call');
-  // }
 
   @override
   Widget build(BuildContext context) {
